6eadc6be92b3832285d4f1fca23f639b05877aa2
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-08-11 20:49:05 +0200

    Assignement Finally Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards() {
+    const emptyArray = [];
+  for (let i = 0; i < names.length; i++) {
+    emptyArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return emptyArray;
+}
+
+console.log(writeCards());
+
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  countDown(10); // This will log the numbers from 10 down to 0
+  
